fd7f49d1446ee38e12d205c50e514fe851a10075
eugene-fedurkin <eugene.fedurkin@gmail.com>
2017-09-21 17:10:47 +0300

    move functions into zeros func

--- src/index.js
+++ src/index.js
@@ -8,6 +8,72 @@ module.exports = function zeros(expression) {
     result = multiply(factorials[i], result);
   }
   return result.length - result.match(/0{0,}$/).index;
+
+  function getFactorial(numb) {
+    let result = numb;
+    if (numb.slice(-2) === '!!' && Number(numb.slice(0, -2)) % 2 === 0) {
+      result = result.slice(0, -2);
+      for (let factor = 2; factor < Number(numb.slice(0, -2)); factor += 2) {
+        result = multiply(`${factor}`, result);
+      }
+    } else if (numb.slice(-2) === '!!' && Number(numb.slice(0, -2)) % 2 === 1) {
+      result = result.slice(0, -2);
+      for (let factor = 1; factor < Number(numb.slice(0, -2)); factor += 2) {
+        result = multiply(`${factor}`, result);
+      }
+    } else {
+      result = result.slice(0, -1);
+      for (let factor = 1; factor < Number(numb.slice(0, -1)); factor++) {
+        result = multiply(`${factor}`, result);
+      }
+    }
+    return result;
+  }
+  
+  function multiply(multiplicand, factor) {
+    let products = [];
+    for (let digitIndex = multiplicand.length - 1; digitIndex >= 0; digitIndex--) {
+      let product = 0;
+      for (let sumIndex = 0; sumIndex < +multiplicand[digitIndex]; sumIndex++) {
+        product = add(product, factor);
+      }
+  
+      for (let zeroIndex = 0; zeroIndex < multiplicand.length - 1 - digitIndex; zeroIndex++) {
+        product = product + '0';
+      }
+  
+      products.push(product);
+    }
+  
+    let result = 0;
+    for (let product of products) {
+      result = add(result, product);
+    }
+  
+    return result;
+  }
+  
+  function add(first, second) {
+    let result = '0';
+    let digits = [];
+    let shift = false;
+    let maxLength = first.length > second.length ? first.length : second.length;
+    let sum;
+  
+    for (let index = 0; index < maxLength; index++) {
+      let firstDigit = first[first.length - 1 - index] ? +first[first.length - 1 - index] : 0;
+      let secondDigit = second[second.length - 1 - index] ? +second[second.length - 1 - index] : 0;
+  
+      sum = firstDigit + secondDigit;
+      sum += shift ? 1 : 0;
+      digits.unshift(sum % 10);
+      shift = sum > 9;
+    }
+  
+    result = digits.join('');
+    shift ? result = '1' + result : result;
+    return result;
+  }
 }
 
 function zerosRecursive(expression) {
@@ -31,72 +97,6 @@ function getFactorialRecursive(argument, skip) {
   if (+argument < 1) return '1';
   let nextValue = skip ? decrement(decrement(argument)) : decrement(argument);
   return multiply(argument, getFactorialRecursive(nextValue, skip));
-}
-
-function getFactorial(numb) {
-  let result = numb;
-  if (numb.slice(-2) === '!!' && Number(numb.slice(0, -2)) % 2 === 0) {
-    result = result.slice(0, -2);
-    for (let factor = 2; factor < Number(numb.slice(0, -2)); factor += 2) {
-      result = multiply(`${factor}`, result);
-    }
-  } else if (numb.slice(-2) === '!!' && Number(numb.slice(0, -2)) % 2 === 1) {
-    result = result.slice(0, -2);
-    for (let factor = 1; factor < Number(numb.slice(0, -2)); factor += 2) {
-      result = multiply(`${factor}`, result);
-    }
-  } else {
-    result = result.slice(0, -1);
-    for (let factor = 1; factor < Number(numb.slice(0, -1)); factor++) {
-      result = multiply(`${factor}`, result);
-    }
-  }
-  return result;
-}
-
-function multiply(multiplicand, factor) {
-  let products = [];
-  for (let digitIndex = multiplicand.length - 1; digitIndex >= 0; digitIndex--) {
-    let product = 0;
-    for (let sumIndex = 0; sumIndex < +multiplicand[digitIndex]; sumIndex++) {
-      product = add(product, factor);
-    }
-
-    for (let zeroIndex = 0; zeroIndex < multiplicand.length - 1 - digitIndex; zeroIndex++) {
-      product = product + '0';
-    }
-
-    products.push(product);
-  }
-
-  let result = 0;
-  for (let product of products) {
-    result = add(result, product);
-  }
-
-  return result;
-}
-
-function add(first, second) {
-  let result = '0';
-  let digits = [];
-  let shift = false;
-  let maxLength = first.length > second.length ? first.length : second.length;
-  let sum;
-
-  for (let index = 0; index < maxLength; index++) {
-    let firstDigit = first[first.length - 1 - index] ? +first[first.length - 1 - index] : 0;
-    let secondDigit = second[second.length - 1 - index] ? +second[second.length - 1 - index] : 0;
-
-    sum = firstDigit + secondDigit;
-    sum += shift ? 1 : 0;
-    digits.unshift(sum % 10);
-    shift = sum > 9;
-  }
-
-  result = digits.join('');
-  shift ? result = '1' + result : result;
-  return result;
 }
 
 function decrement(argument) {
